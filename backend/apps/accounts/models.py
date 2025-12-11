39616970dd538bbb318dc8ac7573f41626515ec7
BoberSmallKing <boberddsdff@gmail.com>
2025-12-11 12:45:57 +0200

    Located in backend folder

---
+++ backend/apps/accounts/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    full_name = models.CharField(max_length=200)
+    bio = models.TextField(max_length=500)
+    number = models.DecimalField(max_digits=11, decimal_places=0, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    
+    USERNAME_FIELD = 'number'
+    REQUIRED_FIELDS = ['username']
+    
+    class Meta:
+        db_table = 'users'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+    
+    def __str__(self):
+        return self.full_name
+
+
+
---
+++ backend/apps/accounts/serializers.py
@@ -0,0 +1,64 @@
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+from django.contrib.auth.password_validation import validate_password
+from .models import User
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        validators=[validate_password]
+    )
+    password_confirm = serializers.CharField(write_only=True)
+    
+    class Meta:
+        model = User
+        fields = ("username", "full_name", "bio", "number", "password", "password_confirm")
+        
+    
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password_confirm"]:
+            raise serializers.ValidationError(
+                {'password': 'Password field didntn match.'}
+            )
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+        user = User.objects.create_user(**validated_data)
+        return user
+    
+    
+class UserLoginSerializer(serializers.Serializer):
+    number = serializers.DecimalField(max_digits=11, decimal_places=0)
+    password = serializers.CharField(write_only=True)
+    
+    def validate(self, attrs):
+        number = attrs.get('number')
+        password = attrs.get('password')
+        
+        if number and password:
+            user = authenticate(
+                request=self.context.get('request'),
+                username=number,
+                password=password
+            )
+            if not user:
+                raise serializers.ValidationError(
+                    'User no found"!'
+                )
+            if not user.is_active:
+                raise serializers.ValidationError(
+                    'User account is disable!'
+                )
+            attrs['user'] = user
+            return attrs
+        else:
+            raise serializers.ValidationError(
+                'Must include "email" and "password"!'
+            )
+            
+            
+            
+
+        
+    
